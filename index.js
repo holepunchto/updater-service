47e28477ee88f1daf1ad69f4fcab5ae125420646
Marco <maidh91@gmail.com>
2025-04-15 22:27:49 +1000

    clean

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const debounceify = require('debounceify')
 
 const isDev = Pear.config.key === null
 
+const READY_MSG = 'ready'
+
+//
+// main
+//
+
 function main (botPath) {
   const runLink = isDev ? botPath : new URL(botPath, `${Pear.config.applink}/`).href
 
@@ -11,11 +17,28 @@ function main (botPath) {
 
   Pear.updates(debounceify(async () => {
     await worker.ready
-    await worker.close()
+    worker.close()
+    await worker.closed
     worker = runWorker(runLink)
     await worker.ready
   }))
 }
+
+function runWorker (runLink) {
+  const pipe = Pear.worker.run(runLink, Pear.config.args)
+  const ready = new Promise((resolve) => {
+    pipe.on('data', (data) => data.toString() === READY_MSG && resolve())
+  })
+  const closed = new Promise((resolve) => {
+    pipe.on('close', () => resolve())
+  })
+  const close = () => pipe.end()
+  return { ready, closed, close }
+}
+
+//
+// worker
+//
 
 function run (botHandler) {
   const bot = botHandler(Pear.config.args, {
@@ -23,21 +46,15 @@ function run (botHandler) {
   })
   const pipe = Pear.worker.pipe()
   if (pipe) {
-    bot.then(() => pipe.write('ready'))
-    pipe.on('end', () => bot.then(({ close }) => close()))
+    bot.then(() => pipe.write(READY_MSG))
+    pipe.on('end', () => bot.then((res) => {
+      if (typeof res === 'object' && 'close' in res && typeof res.close === 'function') {
+        res.close()
+        return
+      }
+      console.error('Missing close function')
+    }))
   }
-}
-
-function runWorker (runLink) {
-  const pipe = Pear.worker.run(runLink, Pear.config.args)
-  const ready = new Promise((resolve) => {
-    pipe.on('data', (data) => data.toString() === 'ready' && resolve())
-  })
-  const close = () => new Promise((resolve) => {
-    pipe.on('close', resolve)
-    pipe.end()
-  })
-  return { ready, close }
 }
 
 module.exports = { main, run }
