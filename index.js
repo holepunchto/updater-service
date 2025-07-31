6f2a12c17a5bff012f31fe15d6ac4e1c9d191e4e
Marco <maidh91@gmail.com>
2025-08-01 01:03:35 +1000

    start worker with fork and length

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 /* global Pear */
-
 const debounceify = require('debounceify')
-
-const isDev = Pear.config.key === null
 
 const READY_MSG = 'ready'
 const CLOSE_MSG = 'close'
+const VERSION_MSG_PREFIX = 'Version:'
 
 //
 // main
 //
 
 function main (botPath) {
-  const runLink = isDev ? botPath : new URL(botPath, `${Pear.config.applink}/`).href
+  let fork = Pear.config.fork
+  let length = Pear.config.length
+  let workerVersion = `${fork}.${length}`
+  let worker = startWorker(getLink(botPath, fork, length))
 
-  let worker = runWorker(runLink)
-
-  const sub = Pear.updates(debounceify(async () => {
+  const debouncedRestart = debounceify(async () => {
+    if (workerVersion === `${fork}.${length}`) return
     await worker.ready
     worker.close()
     await worker.closed
-    worker = runWorker(runLink)
+    worker = startWorker(getLink(botPath, fork, length))
     await worker.ready
-  }))
+    workerVersion = await worker.version
+  })
+
+  const sub = Pear.updates((update) => {
+    fork = update.version.fork
+    length = update.version.length
+    debouncedRestart()
+  })
   Pear.teardown(() => sub.destroy())
 }
 
-function runWorker (runLink) {
+function getLink (botPath, fork, length) {
+  if (Pear.config.key === null) return botPath // dev mode
+
+  const url = new URL(botPath, `${Pear.config.applink}/`)
+  url.host = `${fork}.${length}.${url.host}`
+  return url.href
+}
+
+function startWorker (runLink) {
   const pipe = Pear.worker.run(runLink, Pear.config.args)
   pipe.on('error', (err) => {
     if (err.code === 'ENOTCONN') return
     throw err
   })
+
   const ready = new Promise((resolve) => {
     pipe.on('data', (data) => data.toString() === READY_MSG && resolve())
     pipe.on('close', () => resolve())
@@ -39,8 +55,19 @@ function runWorker (runLink) {
   const closed = new Promise((resolve) => {
     pipe.on('close', () => resolve())
   })
+  const version = new Promise((resolve) => {
+    pipe.on('data', (data) => {
+      const msg = data.toString()
+      if (msg.startsWith(VERSION_MSG_PREFIX)) {
+        const res = msg.split(' ')[1]
+        resolve(res)
+      }
+    })
+  })
+
   const close = () => pipe.write(CLOSE_MSG)
-  return { ready, closed, close }
+
+  return { ready, closed, version, close }
 }
 
 //
@@ -50,7 +77,11 @@ function runWorker (runLink) {
 function run (botHandler) {
   const bot = botHandler(Pear.config.args)
   bot.catch(console.log)
+
   const pipe = Pear.worker.pipe()
+  if (!pipe) return
+
+  pipe.write(`${VERSION_MSG_PREFIX} ${Pear.config.fork}.${Pear.config.length}`)
   bot.then(() => pipe.write(READY_MSG))
   pipe.on('data', (data) => {
     if (data.toString() === CLOSE_MSG) {
