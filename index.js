76bd0c31f02455345d0e684ffa24ca8663e4ed67
Marco <maidh91@gmail.com>
2025-09-01 10:24:41 +1000

    DELAY_UPDATE 60s

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const debounceify = require('debounceify')
 // main
 //
 
+const DELAY_UPDATE = 60000
+
 function main (botPath) {
   let fork = Pear.config.fork
   let length = Pear.config.length
@@ -16,7 +18,7 @@ function main (botPath) {
   Pear.teardown(() => worker.close())
 
   const debouncedRestart = debounceify(async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000)) // wait for the final update
+    await new Promise(resolve => setTimeout(resolve, DELAY_UPDATE)) // wait for the final update
     if (workerVersion === `${fork}.${length}`) return
     console.log(`Updating worker from ${workerVersion} to ${fork}.${length}`)
     await worker.ready
